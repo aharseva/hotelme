97440188b890bfe4424f17fb1fe9715bc3a1018c
Adriana Harseva <a.harseva@gmail.com>
2022-10-29 15:05:14 +0100

    created hotel model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "Creating 10 Hotels"
+10.times do |i|
+  hotel = Hotel.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.address
+    price: rand(50...300)
+    rating: rand(1..5)
+    occupancy: rand(1..8)
+    description: Faker::Quote.famous_last_words
+  )
+  hotel.save!
+end
+puts "Finished!"
