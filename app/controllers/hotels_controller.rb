6f99b4d8abf800e6dae6bcd80e7bcb20593041eb
Adriana Harseva <a.harseva@gmail.com>
2022-10-29 16:04:21 +0100

    added a new page for creating a hotel

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -8,4 +8,18 @@ class HotelsController < ApplicationController
     @hotel = Hotel.find(params[:id])
   end
 
+  def new
+    @hotel = Hotel.new
+  end
+
+  def create
+    @hotel = Hotel.new(hotel_params)
+    @hotel.save
+  end
+
+  private
+
+  def flat_params
+    params.require(:hotel).permit(:name, :address, :description, :price, :occupancy)
+  end
 end
